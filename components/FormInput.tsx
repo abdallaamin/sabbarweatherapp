dc7ad40b5ca065343d18af3477372e3df8411ea0
abdalla-amin <abdalla.amin@vodafone.com>
2023-03-23 03:05:46 +0200

    working graph

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,12 +1,37 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Chart as ChartJS, registerables } from 'chart.js';
+import { Line } from "react-chartjs-2";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-const MyForm = () => {
-    const [Latitude, setLatitude] = useState('');
-    const [Longitude, setLongitude] = useState('');
-    const [city, setCity] = useState('');
-    const [DateRange, setDateRange] = useState('');
-    const [checkbox1, setCheckbox1] = useState(false);
-    const [checkbox2, setCheckbox2] = useState(false);
+ChartJS.register(...registerables);
+
+interface FormComponentProps {
+    index: number;
+}
+
+
+
+
+const WeatherForm = ({ index, }: FormComponentProps) => {
+    const [latitude, setLatitude] = useState<number>(0);
+    const [longitude, setLongitude] = useState<number>(0);
+    const [city, setCity] = useState<string>('');
+    const [temperature, setTemperature] = useState<boolean>(false);
+    const [humidity, setHumidity] = useState<boolean>(false);
+    const [startDate, setStartDate] = useState<Date | null>(new Date());
+    const [endDate, setEndDate] = useState<Date | null>(new Date());
+    const [data, setData] = useState({  labels: [] , datasets: [] });
+
+
+    
+
+    const onChangeDate = (dates:any) => {
+        const [start, end] = dates;
+        setStartDate(start);
+        setEndDate(end);
+    };
 
     const handleLatitudeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setLatitude(event.target.value);
@@ -19,22 +44,64 @@ const MyForm = () => {
     const handlecityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setCity(event.target.value);
     };
-
-    const handleDateRangeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setDateRange(event.target.value);
-    };
-    const handleCheckbox1Change = () => {
-        setCheckbox1(!checkbox1);
+    const handleTemperatureChange = () => {
+        setTemperature(!temperature);
     };
 
-    const handleCheckbox2Change = () => {
-        setCheckbox2(!checkbox2);
+    const handleHumidity2Change = () => {
+        setHumidity(!humidity);
     };
+
+    useEffect(() => {
+        const fetchWeatherData = async () => {
+            try {
+                const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&city=${city}&hourly=${temperature ? 'temperature_2m' : ''}${humidity ? ',relativehumidity_2m' : ''}&start_date=${startDate.toISOString().slice(0, 10)}&end_date=${endDate.toISOString().slice(0, 10)}`
+                console.log(url)
+                const response = await axios.get(url);
+                const labels = response.data.hourly.time.map(date => new Date(date).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric' }) + ' ' + new Date(date).toLocaleString('en-US', { weekday: 'short' }));
+                console.log(labels)
+                const temperatureData = response.data.hourly.temperature_2m;
+                const humidityData = response.data.hourly.relativehumidity_2m;
+
+                const datasets = [];
+
+                if (temperature) {
+                    datasets.push({
+                        label: 'Temperature',
+                        data: temperatureData,
+                        fill: true,
+                        borderColor: 'rgb(75, 192, 192)',
+                        tension: 0.1,
+
+                    });
+                }
+
+                if (humidity) {
+                    datasets.push({
+                        label: 'Humidity',
+                        data: humidityData,
+                        fill: true,
+                        borderColor: 'rgb(255, 99, 132)',
+                        tension: 0.1,
+                    });
+                }
+
+                setData( {  labels ,datasets} );
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchWeatherData();
+
+    }, [latitude, longitude, temperature, humidity, city, startDate, endDate]);
+
+
 
     return (
+        <>
         <div className="flex flex-col justify-center pt-5">
-            <div className="flex flex-row w-full  max-w-full">
-                <div className="w-1/2 pr-4">
+            <div className="flex flex-row w-full  max-w-full" key={index}>
+                <div className="w-1/2 pr-4" >
                     <label className="block font-medium text-gray-700 mb-2" htmlFor="input1">
                         Latitude
                     </label>
@@ -43,7 +110,7 @@ const MyForm = () => {
                         id="input1"
                         type="number"
                         placeholder="Enter Latitude"
-                        value={Latitude}
+                        value={latitude}
                         onChange={handleLatitudeChange}
                     />
                 </div>
@@ -57,7 +124,7 @@ const MyForm = () => {
                         id="input2"
                         type="number"
                         placeholder="Enter Longitude"
-                        value={Longitude}
+                        value={longitude}
                         onChange={handleLongitudeChange}
                     />
                 </div>
@@ -80,14 +147,17 @@ const MyForm = () => {
                     <label className="block font-medium text-gray-700 mb-2" htmlFor="input4">
                         Date Range
                     </label>
-                    <input
+                    <div>
+                    <DatePicker
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="input4"
-                        type="date"
-                        placeholder="Enter Date range"
-                        value={DateRange}
-                        onChange={handleDateRangeChange}
+                        selected={startDate}
+                        onChange={onChangeDate}
+                        startDate={startDate}
+                        endDate={endDate}
+                        selectsRange
+                        //isClearable
                     />
+                    </div>
                 </div>
             </div>
             <div className="flex flex-row w-1/2 mb-4 mt-3">
@@ -96,8 +166,8 @@ const MyForm = () => {
                         <input
                             type="checkbox"
                             className="form-checkbox h-5 w-5 text-black"
-                            checked={checkbox1}
-                            onChange={handleCheckbox1Change}
+                            checked={temperature}
+                            onChange={handleTemperatureChange}
                         />
                         <span className="ml-2 text-gray-700">Temperature </span>
                     </label>
@@ -107,17 +177,24 @@ const MyForm = () => {
                         <input
                             type="checkbox"
                             className="form-checkbox h-5 w-5 text-gray-600"
-                            checked={checkbox2}
-                            onChange={handleCheckbox2Change}
+                            checked={humidity}
+                            onChange={handleHumidity2Change}
                         />
                         <span className="ml-2 text-gray-700">Relative Humidity</span>
                     </label>
                 </div>
+                <div className='flex justify-end pt-4'>
+                    <button className="w-full rounded-lg bg-gray-600 hover:bg-black text-white" onClick={() => console.log("saved to repoprt")}>Save</button>
+                </div>
             </div>
         </div>
-        
-
+            <div className="flex justify-center">
+                <div className="w-3/4 h-1/2">
+                    <Line data={data} />
+                </div>
+            </div>
+        </>
     );
 };
 
-export default MyForm;
+export default WeatherForm;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,38 @@
+import { useState, useLayoutEffect } from 'react'
 import type { NextPage } from 'next'
 import FormInput from '../components/FormInput'
-
-
+interface ComponentData {
+  id: number;
+}
 const Home: NextPage = () => {
+  // const [addNew, setaddNew] = useState<boolean>(false);
+  const [components, setComponents] = useState<ComponentData[]>([]);
+
+  const addComponent = () => {
+    const newId = components.length + 1;
+    const newComponent: ComponentData = {
+      id: newId,
+    };
+    setComponents([...components, newComponent]);
+  };
+
+
   return (
     <div className="flex justify-center h-screen">
       <div className='pt-6'>
         <h1 className="font-bold text-lg">Select coordinates or city </h1>
-        <FormInput />
-        <div className='flex justify-end'>
-          <button className="w-1/12 rounded-lg bg-orange-200" onClick={()=> console.log("its clicked add another city ")}>+</button>
-        </div>
+        <FormInput  />
+        <div className="flex justify-center "></div>
+          <button className="w-1/12 rounded-lg bg-orange-200" onClick={addComponent}>+</button>
+          {components.map((component) => (
+            <div className="mx-auto ">
+              <FormInput key={component.id} index={component.id} />
+              </div>
+          ))}
         <div className='flex justify-end pt-4'>
           <button className="w-1/6 rounded-lg bg-gray-600 hover:bg-black text-white" onClick={() => console.log("saved to repoprt")}>Save</button>
         </div>
       </div>
-      
     </div>
   )
 }
